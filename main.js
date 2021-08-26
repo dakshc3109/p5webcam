e231e099af429fcd55585d863a16376e7bc18333
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-08-26 16:40:09 +0530

    completing project

--- main.js
+++ main.js
@@ -1,25 +1,26 @@
+var video; 
 function preload(){
     
-};
+}
 
 function setup(){
  var canvas = createCanvas(640, 480);
  canvas.position(110, 250);
- var video = createCapture(VIDEO);
+ video = createCapture(VIDEO);
  video.hide();
 
  tint_color = ""; 
-};
+}
 
 function draw(){
     image(video, 0, 0, 640, 480);
     tint(tint_color);
-};
+}
 
 function take_snapshot(){
     save('student_name.png');
-};
+}
 
 function filter_tint(){
     tint_color = document.getElementById("color_name").value;
-};
+}
